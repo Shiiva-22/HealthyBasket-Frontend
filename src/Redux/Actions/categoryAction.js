5dd0282e1dc12b197c9bdf04b287a50265812555
Shiva singh <skweb086@gmail.com>
2025-04-24 00:50:28 +0530

    Update categoryAction.js

--- src/Redux/Actions/categoryAction.js
+++ src/Redux/Actions/categoryAction.js
@@ -13,7 +13,8 @@ import {
   UPDATE_CATEGORY_REQUEST,
   UPDATE_CATEGORY_SUCCESS,
 } from "../Constants/categoryConstants";
-import axios from '../../../axiosInstance';
+import axios from "../utils/axiosInstance";
+
 
 
 export const addCategoryAction = (categoryData) => async (dispatch) => {
